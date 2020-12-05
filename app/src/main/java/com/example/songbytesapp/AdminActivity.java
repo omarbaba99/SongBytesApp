b90d5cdc820de68a89afc5a39769a459b65b4d5b
Parth_V <parthvashisht20@gmail.com>
2020-12-05 19:04:34 +0530

    Activity for adding songs included

--- app/src/main/java/com/example/songbytesapp/AdminActivity.java
+++ app/src/main/java/com/example/songbytesapp/AdminActivity.java
@@ -2,7 +2,9 @@ package com.example.songbytesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+    }
+
+    public void openAddSongActivity(View view){
+        Intent intent = new Intent(this, AddSongActivity.class);
+        startActivity(intent);
     }
 }
