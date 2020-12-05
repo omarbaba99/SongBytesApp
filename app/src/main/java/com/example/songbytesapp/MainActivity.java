3832262e358db43c2b16b76e8e656147a46fbce1
Parth_V <parthvashisht20@gmail.com>
2020-12-05 18:59:51 +0530

    Added new activities for admin and user

--- app/src/main/java/com/example/songbytesapp/MainActivity.java
+++ app/src/main/java/com/example/songbytesapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.songbytesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickLogin(View view) {
+        EditText editEmail = (EditText) findViewById(R.id.editTextEmail);
+        EditText editPassword = (EditText) findViewById(R.id.editTextPassword);
+        String email = editEmail.getText().toString();
+        String password = editPassword.getText().toString();
+        if(email.equals("admin@gmail.com") && password.equals("admin"))
+        {
+            openAdminActivity();
+        }
+        else if(email.equals("user@gmail.com") && password.equals("user")){
+            openUserActivity();
+        }
+        else {
+            Toast.makeText(this, "Invalid Username or Password!" , Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void openUserActivity(){
+        Intent intent = new Intent(this, SongsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openAdminActivity(){
+        Intent intent = new Intent(this, AdminActivity.class);
+        startActivity(intent);
     }
 }
